07ae4efdfb4bc508cfd581084e0d0b77bca20b86
Inês Alves <inesalvescloliveira@ua.pt>
2026-06-21 16:39:37 +0100

    .

---
+++ Assets/ModalWindowPanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    [SerializeField] private GameObject popupPanel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowPopup()
+    {
+        popupPanel.SetActive(true);
+    }
+
+    public void HidePopup()
+    {
+        popupPanel.SetActive(false);
+    }
+}
---
+++ Assets/florPop.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class florPop : MonoBehaviour
+{
+    [Tooltip("Assign the Info_liblinha Canvas GameObject here")]
+    public GameObject infoCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("florPop: Player entered trigger - showing Info_liblinha");
+            if (infoCanvas != null) infoCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("florPop: Player exited trigger - hiding Info_liblinha");
+            if (infoCanvas != null) infoCanvas.SetActive(false);
+        }
+    }
+
+    // 2D physics support
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("florPop (2D): Player entered trigger - showing Info_liblinha");
+            if (infoCanvas != null) infoCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("florPop (2D): Player exited trigger - hiding Info_liblinha");
+            if (infoCanvas != null) infoCanvas.SetActive(false);
+        }
+    }
+}
